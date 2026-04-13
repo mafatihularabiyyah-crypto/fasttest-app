7fecaa4bfd531ec7289ae0483629014c4e70fdcc
TarbiyahTech <mafatihularabiyyah.com>
2026-04-14 00:08:21 +0700

    maitenance error

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  // Jika Anda ingin menggunakan Cloudflare Pages, aktifkan ini:
+  // output: 'export', 
 };
 
 export default nextConfig;
